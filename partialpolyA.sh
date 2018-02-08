10a24e946707783764621be858b1fe8c238a6e28
CnrLwlss <conor.lawless@ncl.ac.uk>
2018-02-08 10:08:30 +0000

    Add papers, update alignment script

--- partialpolyA.sh
+++ partialpolyA.sh
@@ -1,24 +1,43 @@
-export PATH=$PATH
+# Install bowtie2 and samtools:
+# apt-get install bowtie2 samtools
+# Install cutadapt:
+# pip3 install cutadapt
+# Install SRA toolkit:
+# https://github.com/ncbi/sra-tools/wiki/HowTo:-Binary-Installation
 
-root='GSE48933'
-chrlen=${#root}
-subr=${root:0:(chrlen-3)}
+root='SRR935452'
+#root='SRR935453'
 
-# Download...
-#wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/${subr}nnn/${root}/suppl/${root}_RAW.tar
+adapt_Rooijers='TCGTATGCCGTCTTCTGCTTG'
+adapt_Gao='TGGAATTCTCGGGTGCCAAGGAACTCCAGTCACATCACGATCTCGTATGCCGTCTTCTGCTTG'
 
-# Untar...
-mkdir -p ${root}
-tar -xvf ${root}_RAW.tar --directory ${root}
-# Remove adapter
-#cutadapt -a ????? -O 12 -m ????.fastq -o ????_trimmed.fastq
+# Collect sequences and build index of human rRNA and tRNA 
+#wget ftp://ftp.ensembl.org/pub/release-90/fasta/homo_sapiens/ncrna/Homo_sapiens.GRCh38.ncrna.fa.gz
+#gunzip Homo_sapiens.GRCh38.ncrna.fa.gz
+#awk '/^>/ {P=($0~"gene_biotype:rRNA")||($0~"gene_biotype:rRNA_pseudogene")||($0~"gene_biotype:Mt_rRNA")} {if(P) print} ' Homo_sapiens.GRCh38.ncrna.fa > Homo_sapiens.GRCh38.rrna.fa
+#wget http://gtrnadb.ucsc.edu/genomes/eukaryota/Hsapi19/hg19-tRNAs.fa
+#cat Homo_sapiens.GRCh38.rrna.fa hg19-tRNAs.fa > hgRNA.fa
+#mkdir hgRNA
+#bowtie2-build hgRNA.fa hgRNA/hgRNA
 
-# Align reads to decoy Trna/Rrna from nuclear and MT genome
-#bowtie2 -p 4 -D20 -R 10 -N 1 -L 20 -i C,1 --un screened.fastq -x prescreen -U ????_trimmed.fastq -S screened_out.sam
+# Collect sequences and build index of human mtDNA
+#curl "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=NC_012920.1&rettype=fasta&retmode=text" >mtDNA.fa
+#mkdir mtDNA
+#bowtie2-build mtDNA.fa mtDNA/mtDNA
+
+#fastq-dump ${root}
+
+# For SRR935453: "One or more adapter sequences may be incomplete: 91% of the bases preceding the sequence are C.  Add C at start of adapter?
+# Should there be two adapters?  One 3' and one 5'?
+cutadapt -a ${adapt_Rooijers} -O 12 -m 20 -j 5 ${root}.fastq -o ${root}_trimmed.fastq
+
+# Align reads to decoy Trna/Rrna from nuclear and MT genome (4% alignment?)
+bowtie2 -p 5 -D20 -R 10 -N 1 -L 20 -i C,1 --un screened.fastq -x hgRNA/hgRNA -U ${root}_trimmed.fastq -S screened_out.sam
 
 # Align to mitochondrial genome
-#bowtie2 -p 10 -D20 -R 10 -N 1 -L 20 -i C,1 -x HumanMT -U screened.fastq -Q ALIGNMITO.sam
+bowtie2 -p 5 -D20 -R 10 -N 1 -L 20 -i C,1 -x mtDNA/mtDNA -U screened.fastq -S ALIGNMITO.sam
 
 # Filter out multi mapping reads, convert to a sorted BAM file
 #samtools view -q 1 -bS screened_out.sam -u|samtools sort - -f screened.sorted.bam
-
+#samtools view -q 1 -S screened_out.sam -u|samtools sort - -f screened.sorted.sam
+#samtools view -q 1 -S screened_out.sam
